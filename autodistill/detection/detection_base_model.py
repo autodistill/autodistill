c3a6d58f2a4bcdc8655234e4b19bfecec075a8e0
James Gallagher <jamesg@jamesg.blog>
2023-06-13 10:15:47 +0100

    add roboflow upload to detection base model

--- autodistill/detection/detection_base_model.py
+++ autodistill/detection/detection_base_model.py
@@ -1,16 +1,16 @@
+import glob
+import os
 from abc import abstractmethod
 from dataclasses import dataclass
 
-import os
-import glob
-from tqdm import tqdm
 import cv2
-
 import supervision as sv
 from autodistill.core import BaseModel
 from autodistill.detection import DetectionOntology
-
 from autodistill.helpers import split_data
+from roboflow import Roboflow
+from tqdm import tqdm
+
 
 @dataclass
 class DetectionBaseModel(BaseModel):
@@ -20,10 +20,21 @@ class DetectionBaseModel(BaseModel):
     def predict(self, input: str) -> sv.Detections:
         pass
 
-    def label(self, input_folder: str, extension: str = ".jpg", output_folder: str = None) -> sv.DetectionDataset:
+    def label(
+        self,
+        input_folder: str,
+        extension: str = ".jpg",
+        output_folder: str = None,
+        human_in_the_loop: bool = False,
+        roboflow_api_key: str = None,
+        roboflow_workspace: str = None,
+        roboflow_project: str = None,
+        roboflow_tags: str = [],
+        roboflow_batch_name: str = None,
+    ) -> sv.DetectionDataset:
         if output_folder is None:
             output_folder = input_folder + "_labeled"
-        
+
         os.makedirs(output_folder, exist_ok=True)
 
         images_map = {}
@@ -54,6 +65,24 @@ class DetectionBaseModel(BaseModel):
 
         split_data(output_folder)
 
-        print("Labeled dataset created - ready for distillation.")  
-        return dataset  
+        if (
+            human_in_the_loop
+            and roboflow_api_key is not None
+            and roboflow_workspace is not None
+            and roboflow_project is not None
+        ):
+            rf = Roboflow(api_key=roboflow_api_key)
+            workspace = rf.workspace(roboflow_workspace)
+            project = workspace.project(roboflow_project)
 
+            datasets = ["train", "test", "valid"]
+
+            if roboflow_batch_name is None:
+                raise ValueError("roboflow_batch_name must be specified")
+
+            for dataset in datasets:
+                for file in glob.glob(os.path.join(output_folder, dataset, "images/*")):
+                    project.upload(file, tags=roboflow_tags, batch=roboflow_batch_name)
+
+        print("Labeled dataset created - ready for distillation.")
+        return dataset
