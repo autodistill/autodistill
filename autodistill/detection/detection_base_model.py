beee2de37e593682aa3bdfed5d6d7599dbc97e72
Thomas Friedel <thomas.friedel@gmail.com>
2023-09-04 21:59:44 +0200

    adapted for https://github.com/roboflow/supervision/pull/353 use LazyLoadDict and shelve for detections to reduce memory consumption

--- autodistill/detection/detection_base_model.py
+++ autodistill/detection/detection_base_model.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 
 import cv2
 import supervision as sv
+from supervision.dataset.utils import LazyLoadDict
 from tqdm import tqdm
 
 from autodistill.core import BaseModel
@@ -12,6 +13,8 @@ from autodistill.helpers import split_data
 
 from .detection_ontology import DetectionOntology
 
+import shelve
+import tempfile
 
 @dataclass
 class DetectionBaseModel(BaseModel):
@@ -29,8 +32,12 @@ class DetectionBaseModel(BaseModel):
 
         os.makedirs(output_folder, exist_ok=True)
 
-        images_map = {}
-        detections_map = {}
+        images_map = LazyLoadDict()
+        # Create a temporary file for the shelve
+        temp_filename = tempfile.mktemp()
+        detections_map = shelve.open(temp_filename)
+
+        print(f"Storing temporary data in shelve file: {temp_filename}")
 
         files = glob.glob(input_folder + "/*" + extension)
         progress_bar = tqdm(files, desc="Labeling images")
@@ -40,10 +47,12 @@ class DetectionBaseModel(BaseModel):
             image = cv2.imread(f_path)
 
             f_path_short = os.path.basename(f_path)
-            images_map[f_path_short] = image.copy()
+            images_map[f_path_short] = f_path
             detections = self.predict(f_path)
             detections_map[f_path_short] = detections
 
+        detections_map.close() # Close the shelve file
+        detections_map = shelve.open(temp_filename, flag='r')
         dataset = sv.DetectionDataset(
             self.ontology.classes(), images_map, detections_map
         )
