b381d9d2695ae3f484c49d4b1a0582075cb6fb13
Steven Schreck <steven.schreck@outlook.de>
2023-10-12 15:59:56 +0200

    Update detection_base_model.py
    Added chunk parameter to label function
    
    - The parameter is meant to split up the files into n chunks.
    - Those chunks are beeing processed which is a possibility to reduce the RAM load of the distillation
    - Chunks will inherit their own data.yaml which is the switch to check if the chunk was already processed. If the labeling is aborted for any reason it can be restarted and it will continue at the latest incomplete chunk.

--- autodistill/detection/detection_base_model.py
+++ autodistill/detection/detection_base_model.py
@@ -8,10 +8,11 @@ import supervision as sv
 from tqdm import tqdm
 
 from autodistill.core import BaseModel
+from autodistill.detection import DetectionOntology
 from autodistill.helpers import split_data
 
-from .detection_ontology import DetectionOntology
-
+import math
+import shutil
 
 @dataclass
 class DetectionBaseModel(BaseModel):
@@ -22,38 +23,75 @@ class DetectionBaseModel(BaseModel):
         pass
 
     def label(
-        self, input_folder: str, extension: str = ".jpg", output_folder: str = None
+        self, input_folder: str, extension: str = ".jpg", output_folder: str = None, chunks: int = 1
     ) -> sv.DetectionDataset:
         if output_folder is None:
             output_folder = input_folder + "_labeled"
 
         os.makedirs(output_folder, exist_ok=True)
+        
+        if not os.path.exists(os.path.join(output_folder, "data.yaml")): # Do the labeling because there are no labels
+            images_map = {}
+            detections_map = {}
 
-        images_map = {}
-        detections_map = {}
+            files = glob.glob(input_folder + "/*" + extension)
+            
+            ######### CHUNK PROCESSING NEW
+            # Compute the size of each chunk
+            chunk_size = math.ceil(len(files) / chunks)
+            
+            all_chunk_folders = []
+            
+            # Break files into smaller chunks for processing
+            for i in range(chunks):
+                chunk_output_folder = f"{output_folder}_chunk_{i+1}"
+                all_chunk_folders.append(chunk_output_folder)
+                os.makedirs(chunk_output_folder, exist_ok=True)
+                
+                if os.path.exists(os.path.join(chunk_output_folder, "data.yaml")):
+                    continue # Skip this chunk because it is already labeled
 
-        files = glob.glob(input_folder + "/*" + extension)
-        progress_bar = tqdm(files, desc="Labeling images")
-        # iterate through images in input_folder
-        for f_path in progress_bar:
-            progress_bar.set_description(desc=f"Labeling {f_path}", refresh=True)
-            image = cv2.imread(f_path)
+                start_idx = i * chunk_size
+                end_idx = min((i + 1) * chunk_size, len(files))
 
-            f_path_short = os.path.basename(f_path)
-            images_map[f_path_short] = image.copy()
-            detections = self.predict(f_path)
-            detections_map[f_path_short] = detections
+                current_files_chunk = files[start_idx:end_idx]
+                progress_bar = tqdm(current_files_chunk, desc=f"Labeling Chunk {i+1}/{chunks}")
 
-        dataset = sv.DetectionDataset(
-            self.ontology.classes(), images_map, detections_map
-        )
+                images_map = {}  
+                detections_map = {} 
 
-        dataset.as_yolo(
-            output_folder + "/images",
-            output_folder + "/annotations",
-            min_image_area_percentage=0.01,
-            data_yaml_path=output_folder + "/data.yaml",
-        )
+                # Process each chunk
+                for f_path in progress_bar:
+                    progress_bar.set_description(desc=f"Labeling Chunk {i+1}/{chunks}: {f_path}", refresh=True)
+                    image = cv2.imread(f_path)
+
+                    f_path_short = os.path.basename(f_path)
+                    images_map[f_path_short] = image.copy()
+                    detections = self.predict(f_path)
+                    detections_map[f_path_short] = detections
+
+                dataset = sv.DetectionDataset(
+                    self.ontology.classes(), images_map, detections_map
+                )
+
+                dataset.as_yolo(
+                    chunk_output_folder + "/images",
+                    chunk_output_folder + "/annotations",
+                    min_image_area_percentage=0.01,
+                    data_yaml_path=chunk_output_folder + "/data.yaml",
+                )
+
+                del images_map
+                del detections_map
+                
+            # After processing all chunks, aggregate data into main output folder
+            for chunk_folder in tqdm(all_chunk_folders, desc="Building final dataset"):
+                for subdir, _, files in os.walk(chunk_folder):
+                    for file_name in files:
+                        source = os.path.join(subdir, file_name)
+                        dest = os.path.join(output_folder, os.path.relpath(subdir, chunk_folder), file_name)
+                        os.makedirs(os.path.dirname(dest), exist_ok=True)
+                        shutil.copy2(source, dest)
 
         split_data(output_folder)
 
