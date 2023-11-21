f230c09035e091396a5d613a3d1cafee17fda8fb
James Gallagher <jamesg@jamesg.blog>
2023-11-21 18:12:08 +0000

    fix dataset splitting bug, add resume labeling logic

--- autodistill/classification/classification_base_model.py
+++ autodistill/classification/classification_base_model.py
@@ -24,18 +24,29 @@ class ClassificationBaseModel(BaseModel):
     def label(
         self, input_folder: str, extension: str = ".jpg", output_folder: str = None
     ) -> sv.ClassificationDataset:
-        if output_folder is None:
-            output_folder = input_folder + "_labeled"
-
-        # check if output folder exists
-        if os.path.exists(output_folder):
-            # if it does, append timestamp to it
-            output_folder += "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-        os.makedirs(output_folder, exist_ok=True)
-
         images_map = {}
         detections_map = {}
+
+        # if output_folder/autodistill.json exists
+        if os.path.exists(output_folder + "/data.yaml"):
+            dataset = sv.ClassificationDataset.from_yolo(
+                output_folder + "/images",
+                output_folder + "/annotations",
+                output_folder + "/data.yaml",
+            )
+
+            # DetectionsDataset iterator returns
+            # image_name, image, self.annotations.get(image_name, None)
+            # ref: https://supervision.roboflow.com/datasets/#supervision.dataset.core.DetectionDataset
+            for item in dataset:
+                image_name = item[0]
+                image = item[1]
+                detections = item[2]
+
+                image_base_name = os.path.basename(image_name)
+
+                images_map[image_base_name] = image
+                detections_map[image_base_name] = detections
 
         files = glob.glob(input_folder + "/*" + extension)
         progress_bar = tqdm(files, desc="Labeling images")
@@ -46,15 +57,19 @@ class ClassificationBaseModel(BaseModel):
 
             f_path_short = os.path.basename(f_path)
             images_map[f_path_short] = image.copy()
-            detections = self.predict(f_path)
-            detections_map[f_path_short] = detections
+
+            annotation_path = os.path.join(output_folder, "annotations/", ".".join(f_path_short.split(".")[:-1]) + ".txt")
+
+            if not os.path.exists(annotation_path):
+                detections = self.predict(f_path)
+                detections_map[f_path_short] = detections
 
         dataset = sv.ClassificationDataset(
             self.ontology.classes(), images_map, detections_map
         )
 
-        train_cs, test_cs = split_data(dataset, split_ratio=0.7)
-        test_cs, valid_cs = split_data(test_cs, split_ratio=0.5)
+        train_cs, test_cs = dataset.split(split_ratio=0.7)
+        test_cs, valid_cs = test_cs.split(split_ratio=0.5)
 
         train_cs.as_folder_structure(root_directory_path=output_folder + "/train")
 
