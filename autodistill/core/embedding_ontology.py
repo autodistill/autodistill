8c2075222212a8641abda6fc5d9dfe255717e916
James Gallagher <jamesg@jamesg.blog>
2023-11-21 17:30:39 +0000

    create EmbeddingModel, respond to feedback

--- autodistill/core/embedding_ontology.py
+++ autodistill/core/embedding_ontology.py
@@ -1,15 +1,21 @@
 from dataclasses import dataclass
-from typing import Any, List, Tuple
+from typing import Any, List
 import numpy as np
 
 from autodistill.core.ontology import Ontology
-
 
 @dataclass
 class EmbeddingOntology(Ontology):
     promptMap: List[str, Any]
     embeddingMap: List[str, np.ndarray]
     cluster: int
+
+    def __init__(self, embeddingMap, cluster = 1):
+        self.embeddingMap = embeddingMap
+        self.cluster = cluster
+        
+        if self.cluster != 1:
+            print("The `cluster` parameter is not currently implemented.")
 
     def process(self, inference_callback):
         if self.embeddingMap:
