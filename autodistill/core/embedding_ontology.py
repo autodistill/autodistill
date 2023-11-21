3dacf765c4bf94e998487bc9703d09a788c83e07
James Gallagher <jamesg@jamesg.blog>
2023-11-21 17:23:38 +0000

    respond to feedback

--- autodistill/core/embedding_ontology.py
+++ autodistill/core/embedding_ontology.py
@@ -7,7 +7,22 @@ from autodistill.core.ontology import Ontology
 
 @dataclass
 class EmbeddingOntology(Ontology):
-    promptMap: List[Tuple[np.ndarray, str]]
+    promptMap: List[str, Any]
+    embeddingMap: List[str, np.ndarray]
+    cluster: int
+
+    def process(self, inference_callback):
+        for prompt, cls in self.promptMap:
+            result = []
+
+            for item in cls:
+                # inference callback should support image paths or np arrays
+                result.append(inference_callback(item))
+
+            # get average of all vectors
+            result = np.mean(result, axis=0)
+
+            self.embeddingMap[prompt] = result
 
     def prompts(self) -> List[np.ndarray]:
         return [prompt for prompt, _ in self.promptMap]
