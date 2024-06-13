3a03e6af6cdbabcd54ba1aeb7c553cc87c3f60bc
James Gallagher <jamesg@jamesg.blog>
2024-06-13 14:48:35 +0100

    fix linter

--- autodistill/text_classification/__init__.py
+++ autodistill/text_classification/__init__.py
@@ -1,6 +1,9 @@
-from autodistill.text_classification.text_classification_base_model import \
-    TextClassificationBaseModel
-from autodistill.text_classification.text_classification_ontology import \
-    TextClassificationOntology
-from autodistill.text_classification.text_classification_target_model import \
-    TextClassificationTargetModel
+from autodistill.text_classification.text_classification_base_model import (
+    TextClassificationBaseModel,
+)
+from autodistill.text_classification.text_classification_ontology import (
+    TextClassificationOntology,
+)
+from autodistill.text_classification.text_classification_target_model import (
+    TextClassificationTargetModel,
+)
